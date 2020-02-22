329958b4e6aa4724437003c08f4ed64294da970f
Balkirat <cheemaballu@gmail.com>
2020-02-22 11:09:17 -0500

    update styling for mobile and tablet for upload page

--- sprint2/src/Pages/Upload.jsx
+++ sprint2/src/Pages/Upload.jsx
@@ -6,7 +6,7 @@ import "./styles.scss";
 function Upload() {
   return (
     <>
-      <h1>UpLoad Video</h1>
+      <h1 className="heading">UpLoad Video</h1>
       <div className="upload-hero">
         <h4 className="upload-hero__subheading">VIDEO THUMBNAIL</h4>
         <video className="upload-hero__video-thumbnail" poster={Thumbnail}></video>
@@ -16,8 +16,10 @@ function Upload() {
         <input className="form__input" placeholder="Add a description of your video"></input>
         <label className="form__label" for="videoDescription">ADD A VIDEO DESCRIPTION</label>
         <input className="form__input--height" placeholder="Add a description of your video"></input>
-        <button className="form__button" >PUBLISH</button>
-        <button className="form__button--noborder">CANCEL</button>
+        <div className="form__wrapper">
+          <button className="form__wrapper__button" >PUBLISH</button>
+          <button className="form__wrapper__button--noborder">CANCEL</button>
+        </div>
       </form>
     </>
   );
